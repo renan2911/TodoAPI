de4928c21d9e653dabd2cee958b79f57ad595dc5
renan2911 <renanalysson2911@hotmail.com>
2020-12-23 15:29:40 -0300

    commit

---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string IsComplete { get; set; }
+    }
+}
